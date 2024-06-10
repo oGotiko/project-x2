c599a210fe6810b6bc5ec347f26354cf7a4acc6a
Astro_815 <astro815sfgKW@gmail.com>
2024-06-10 19:25:35 -0300

    mainhaha

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-    var input_direction = Input.get_vector("left", "right", "up", "down")
-    velocity = input_direction * speed
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
 func _physics_process(_delta):
-    get_input()
-    set_velocity(velocity)
-    move_and_slide()
+	get_input()
+	set_velocity(velocity)
+	move_and_slide()

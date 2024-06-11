593691677066746e2c1649f81c662b7d86390e43
oGotiko <coffewizard@gmail.com>
2024-06-11 01:11:45 -0300

    initial commit 2

--- player.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-# Variáveis de movimento
-
-var velocity = Vector2()
-
-func _physics_process(delta)
-	
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,11 +1,12 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta):
+	get_input()
+	set_velocity(velocity)
+	move_and_slide()
